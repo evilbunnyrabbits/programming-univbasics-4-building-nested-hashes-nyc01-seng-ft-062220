d76178cc77bae979083e1e4b90951470181d4485
beaming-subroutine-5164 <ben.garlock@gmail.com>
2020-05-29 13:35:03 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,7 +1,11 @@
 def base_hash
   # Remember implicit return! We're returning this new Hash _without_ the
   # keyword "return." Nice and neat.
-  Hash.new 
+  my_hash = {
+    :railroads = {}
+  }
+  
+  my_hash
 
 end
 
